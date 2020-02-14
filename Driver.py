9b78c79aee140f5f74eb5e8c45670111278754f2
Anthony Pompili <apompz@yahoo.com>
2020-02-14 17:09:42 -0500

    Initial commit

---
+++ Driver.py
@@ -0,0 +1,12 @@
+# FileName: Driver.py
+# Author: Anthony Pompili
+# Date: February 14, 2020
+
+
+
+'''
+ Description: This file is the main driver for the "Hangman" game. This will initialize the game, and prompt
+ the user if they want to play a game and then create a game for them to play with a phrase.
+
+'''
+
