645ad8b2d61a36682355c02cf7089b51112fc1f1
Anthony Pompili <apompz@yahoo.com>
2020-02-14 19:23:34 -0500

    Added function to take a word or phrase and create a blank string to illustrate the word or phrase the player must guess

--- HangmanGui.py
+++ HangmanGui.py
@@ -7,6 +7,23 @@ class HangmanGui:
 
     def __init__(self, word_to_guess):
         self.word_to_guess = word_to_guess
+        self.word_with_blanks = ""
 
     def print_game_ui(self):
-        print("The word to guess is " + self.word_to_guess)
+        print("The word to guess is " + self.get_blank_word(self.word_to_guess))
+
+    def get_blank_word(self, word_to_guess):
+        blanked_out_word = ""
+
+        for value in range(0, len(word_to_guess)):
+            if word_to_guess[value] == " ":
+                blanked_out_word += "   "
+            else:
+                if value + 1 >= len(word_to_guess):
+                    blanked_out_word += "_"
+                elif word_to_guess[value + 1] == " ":
+                    blanked_out_word += "_"
+                else:
+                    blanked_out_word += "_|"
+
+        return blanked_out_word
